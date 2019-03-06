a8bb265b428c675a8842fb9b23726261e8b89483
Deepak Bawa <bawa_d@ymail.com>
2019-03-06 15:45:48 +0530

    Create app.component.ts

---
+++ app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
+
+import { MenuService } from "./services/menu.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ang-electron';
+  private remote;
+  private menu;
+  private shell;
+
+  constructor( 
+    private _electronService: ElectronService,
+    private _MenuService: MenuService
+  ) {
+
+    this.shell = this._electronService.shell;
+    this.remote = this._electronService.remote;
+    this.menu = this.remote.Menu.buildFromTemplate( this._MenuService.getMenuTemplate() );
+    this.remote.Menu.setApplicationMenu( this.menu );
+  }
+
+  launchWindow() {
+    this.shell.openExternal('https://google.com');
+  }
+}
